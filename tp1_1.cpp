aa3cc1705351a18696868589d331469f33d5cc3d
Isas321 <javier.isas321@gmail.com>
2023-03-24 11:57:00 -0300

    Trabajo con punteros

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main(){
-    printf("hola mundo");
+    int *puntero, variable=1;
+
+    printf("\nhola mundo\n");
+
+    puntero = &variable;
+    printf("\nEl contenido del puntero es: %d \n", *puntero);
+    printf("La direccion almacenada por el puntero es: %p\n", puntero);
+    printf("La direccion de memoria de la variable es: %p\n", &variable);
+    printf("La direccion de memoria del puntero es: %p\n", &puntero);
+    printf("Tamanio de memoria utilizado por la variable es: %d\n", sizeof(variable)); //Como es una variable tipo entero ocupara 4 de almacenamiento
+
     return 0;
 }
